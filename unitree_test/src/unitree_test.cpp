c181f6aaa300cbf3747d97f82558d5ddb2be00db
Yao Xinchen <yao29@illinois.edu>
2024-01-01 14:51:15 +0800

    deleting unecessary files in unitree_controller

--- unitree_test/src/unitree_test.cpp
+++ unitree_test/src/unitree_test.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "unitree_test/unitree_driver.hpp"
 #include <memory>
+#include <rclcpp/timer.hpp>
 
 class UnitreeTest : public rclcpp::Node
 {
@@ -8,10 +9,17 @@ public:
     UnitreeTest() : Node("unitree_controller")
     {
         unitree_driver_ = std::make_unique<UnitreeDriver>();
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&UnitreeTest::timer_callback, this));
     }
 
 private:
-    std::unique_ptr<UnitreeDriver> unitree_driver_; 
+    std::unique_ptr<UnitreeDriver> unitree_driver_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    void timer_callback()
+    {
+        unitree_driver_->set_goal(0, 10);
+    }
 };
 
 int main(int argc, char * argv[])
